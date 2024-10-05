33990ccedc1db8ba1534dcadc9a2818743b80a84
Yury V. Zaytsev <yury@shurup.com>
2024-10-05 11:16:26 +0200

    fixup! WIP
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- lib/tty/color.c
+++ lib/tty/color.c
@@ -48,7 +48,10 @@
 
 /* *INDENT-OFF* */
 static tty_color_pair_t tty_color_defaults = {
-    .fg = NULL, .bg = NULL, .attrs = NULL, .pair_index = 0
+    .fg = NULL,
+    .bg = NULL,
+    .attrs = NULL,
+    .pair_index = 0,
 };
 /* *INDENT-ON* */
 
